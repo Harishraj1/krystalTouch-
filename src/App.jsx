d87938e37d1079922baea762e6b204990559c1d0
Harishraj1 <harishrajd6@gmail.com>
2024-02-11 20:00:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Rating from './components/rating';
+import './style/style.css'
+
+
+
+const App = () => {
+ 
+
+  return (
+    <main>
+    <Rating/>
+
+    </main>
+  );
+};
+
+export default App;
